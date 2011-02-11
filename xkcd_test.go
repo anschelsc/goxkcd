b468c22c96fe97ff6ecefc26bd8746e36bf93c68
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2011-02-11 11:11:25 -0500

    Added test for the "(" comic, which PASSES.

--- xkcd_test.go
+++ xkcd_test.go
@@ -39,3 +39,42 @@ func TestMain(t *testing.T) {
 		t.Errorf("Img should be %s, got %s.", img, c.Img)
 	}
 }
+
+//The following comic can break unsanitized scripts:
+const (
+	wtitle = "("
+	wnum   = 859
+	wyear  = "2011"
+	wmonth = "2"
+	wday   = "11"
+	walt   = `Brains aside, I wonder how many poorly-written xkcd.com-parsing scripts will break on this title (or \\;;"\''{\<<[' this mouseover text."`
+	wimg   = "http://imgs.xkcd.com/comics/(.png"
+)
+
+func TestWeird(t *testing.T) {
+	c, err := Get(wnum)
+	if err != nil {
+		t.Errorf("Error %s returned by Get().", err)
+	}
+	if c.Title != wtitle {
+		t.Errorf("Title should be %s, got %s.", wtitle, c.Title)
+	}
+	if c.Num != wnum {
+		t.Errorf("Num should be %d, got %d.", wnum, c.Num)
+	}
+	if c.Year != wyear {
+		t.Errorf("Year should be %s, got %s.", wyear, c.Year)
+	}
+	if c.Month != wmonth {
+		t.Errorf("Month should be %s, got %s.", wmonth, c.Month)
+	}
+	if c.Day != wday {
+		t.Errorf("Day should be %s, got %s.", wday, c.Day)
+	}
+	if c.Alt != walt {
+		t.Errorf("Alt should be %s, got %s.", walt, c.Alt)
+	}
+	if c.Img != wimg {
+		t.Errorf("Img should be %s, got %s.", wimg, c.Img)
+	}
+}
